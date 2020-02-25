b9c080fc856dfa2b3602419eed562f5265b3e447
Sofia Kyba <56972348+Sofia-Kyba@users.noreply.github.com>
2020-02-25 20:44:26 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -5,7 +5,7 @@
 
 
 def oauth():
-    return {"consumer_key": "YzhXmSqWuGKrxauk2EKnFqT5p",
-            "consumer_secret": "FvSDhK1YISPH5LM7tShElHzYFFyiK0EtySXSPrSdLtPQe0IjcP",
-            "token_key": "1230160462421032960-WMQDN9VS4CkbS9kOkOKBRcGYvlR0r6",
-            "token_secret": "5e5PYH7Ga4UN1oBFm8xQ4YcmcuzgzJlrRLNJXwEUTEO2A"}
+    return {"consumer_key": "Yzh.....FqT5p",
+            "consumer_secret": "FvSD....Qe0IjcP",
+            "token_key": "1230....YvlR0r6",
+            "token_secret": "5e5PY....EO2A"}
